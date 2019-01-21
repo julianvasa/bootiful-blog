470fd04a3ef8306393a84e5a6f50ebe12201cf79
Julian Vasa <vasa.julian@gmail.com>
2019-01-21 21:08:15 +0100

    Removed all "localhost" urls

--- frontend/src/main/java/al/recipes/soap/SoapConfiguration.java
+++ frontend/src/main/java/al/recipes/soap/SoapConfiguration.java
@@ -1,5 +1,6 @@
 package al.recipes.soap;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -9,6 +10,8 @@ import java.net.UnknownHostException;
 
 @Configuration
 public class SoapConfiguration {
+    @Value("${local.server.port}")
+    private int serverPort;
     
     @Bean
     public Jaxb2Marshaller marshaller() {
@@ -19,8 +22,8 @@ public class SoapConfiguration {
     
     @Bean
     public SoapClient categoriesClient(Jaxb2Marshaller marshaller) throws UnknownHostException {
-        String urlConn = "https://" + InetAddress.getLocalHost().getHostName() + ":443";
-        
+        String urlConn = "http://" + InetAddress.getLocalHost().getHostName();
+        System.out.println("JULIIIII " + serverPort);
         SoapClient client = new SoapClient();
         client.setDefaultUri(urlConn + "/ws/categories");
         client.setMarshaller(marshaller);
