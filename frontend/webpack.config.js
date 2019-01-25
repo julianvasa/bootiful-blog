a8cd3f46d7ee75472214953c8c2d3c79394a1aa5
Julian Vasa <vasa.julian@gmail.com>
2019-01-25 19:02:33 +0100

    bug in webpack wrong bundle.js pathv

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     mode: 'development',
     output: {
         path:__dirname,
-        filename: "'src/main/resources/static/built/bundle.js",
+        filename: "src/main/resources/static/built/bundle.js",
         publicPath: '/'
     },
     resolve: {
